bce112225525f44f1583a3d2184bae49aa6e6576
Giovanni Idili <giovanni.idili@gmail.com>
2012-06-25 20:54:23 +0100

    refactoring complete

--- src/test/java/org/openworm/simulationengine/solver/sph/internal/SPHSolverTests.java
+++ src/test/java/org/openworm/simulationengine/solver/sph/internal/SPHSolverTests.java
@@ -237,6 +237,20 @@ public class SPHSolverTests {
 		}
 	}
 	
+	@Test
+	public void testSolve(){
+		// test a full solve step
+		List<List<IModel>> models = solver.solve(generateSampleModel(), null);
+		
+		// check that output is not null and we only got one model back
+		Assert.assertTrue("testSolve: solve output is null", models != null);
+		Assert.assertTrue("testSolve: unexpected number of output models", models.size() == 1);
+		Assert.assertTrue("testSolve: unexpected number of nested output models", models.get(0).size() == 1);
+		// check that number of particles is correct
+		Assert.assertTrue("testSolve: number of particles does not match expected value", ((SPHModelX)models.get(0).get(0)).getNumberOfParticals() == SPHConstants.PARTICLE_COUNT);
+		// TODO: check that the result of integration is correct (same as test integrate)
+	}
+	
 	private boolean testEndClearBuffers(){
 		//test if all ellement of neighborMap is -1
 		boolean result = true;
