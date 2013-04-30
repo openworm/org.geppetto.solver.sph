7b7bf604e67681454afec6237ba7c82ded5089a1
john_idol <giovanni.idili@gmail.com>
2013-04-30 14:46:33 +0100

    re-factored the test and resolved an issue with buffer declaration that was causing memory access errors

--- src/test/java/org/geppetto/solver/sph/internal/KernelTest.java
+++ src/test/java/org/geppetto/solver/sph/internal/KernelTest.java
@@ -4,6 +4,7 @@ import static java.lang.System.out;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.nio.ByteOrder;
 
 import org.bridj.Pointer;
 import org.geppetto.solver.sph.SPHSolverService;
@@ -21,112 +22,121 @@ import com.nativelibs4java.opencl.CLQueue;
 import com.nativelibs4java.opencl.JavaCL;
 import com.nativelibs4java.util.IOUtils;
 
-public class KernelTest
-{
-
-	private CLContext _context;
-	public CLQueue _queue;
-	private CLProgram _program;
-	private CLDevice _device;
-	public CLBuffer<Float> _buffer1;
-	public CLBuffer<Float> _buffer2;
-	private CLKernel _test_kernel;
-	private Pointer<Float> _buffer1Ptr;
-	private Pointer<Float> _buffer2Ptr;
-	private static final int SIZE=10;
+public class KernelTest {
 	
-	private void init(DeviceFeature device) throws IOException  
-	{
-		_context = JavaCL.createBestContext(device);
-		
-		out.println("created "+ _context);
-		// an array with available devices
-		CLDevice[] devices = _context.getDevices();
+	private int buff_size = 10;
+	private float buff1[] = { 0f, 1f, 2f, 3f, 4f, 5f, 6f, 7f, 8f, 9f };
+	private float buff2[] = { 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f };
 
-		for(int i=0; i<devices.length; i++)
-		{
-			out.println("device - " + i + ": " + devices[i]);
-		}	
-
-		// have a look at the output and select a device
-		_device = devices[0];
-		out.println("Version " + _device.getOpenCLVersion());
-		out.println("Version " + _device.getDriverVersion());
-		out.println("using "+ _device);
-		out.println("max workgroup size: " + _device.getMaxWorkGroupSize());
-		out.println("max workitems size: " + _device.getMaxWorkItemSizes()[0]);
+	private void test(DeviceFeature device) throws Exception {
+		FakeSolver fake = new FakeSolver(buff1, buff2, buff_size, device);
 		
-		// create command queue on selected device.
-		_queue = _context.createDefaultQueue();
+		float[] buff2_results = fake.solve();
 		
-		// load sources, create and build program
-		String src = IOUtils.readText(SPHSolverService.class.getResourceAsStream("/testKernel.cl"));
-		_program = _context.createProgram(src);
-		
-	 	_test_kernel = _program.createKernel("test_kernel");
-	 	
-		_buffer1Ptr = Pointer.allocateFloats(SIZE);
-		_buffer2Ptr = Pointer.allocateFloats(SIZE);
-	 	_buffer1 = _context.createBuffer(Usage.InputOutput,_buffer1Ptr,false);
-	 	_buffer2 = _context.createBuffer(Usage.InputOutput, _buffer2Ptr,false);
-	 	
-	 	_queue.finish();
+		for (int i = 0; i < buff_size; i++) {
+			// so we check that every element in the second buffer is equal to
+			// the same element in the first buffer
+			assertEquals(buff1[i], buff2_results[i], 0);
+		}
 	}
 
-	public int runTestKernel(){
-	 	// Stage SortPostPass
-	 	_test_kernel.setArg( 0, _buffer1 );
-	 	_test_kernel.setArg(1, _buffer2 );
-	 	_test_kernel.enqueueNDRange(_queue, new int[] {SIZE});
-	 	return 0;
-	 }
-	
-	private float b1[]={0f,1f,2f,3f,4f,5f,6f,7f,8f,9f};
-	private float b2[]={0f,0f,0f,0f,0f,0f,0f,0f,0f,0f};
-	
-	private void test(DeviceFeature device) throws Exception
-	{
-		init(device);
-		for(int i=0;i<10;i++)
-		{
-			_buffer1Ptr.set(i,b1[i]);
-			_buffer2Ptr.set(i,b2[i]);
-		}
-		
-		//the kernel copies the first buffer in the second one
-		runTestKernel();
-		
-		_buffer1Ptr = _buffer1.read(_queue);
-		_buffer2Ptr = _buffer2.read(_queue);
-		_queue.finish();
-		
-		for(int i=0;i<10;i++)
-		{
-			//so we check that every element in the second buffer is equal to the same element in the first buffer
-			assertEquals(_buffer2Ptr.get(i).floatValue(),b1[i],0);
-		}
-		
-		
+	/*
+	 * TEST JavaCL with CPU
+	 * */
+	@Test
+	public void testKernelCPU() throws Exception {
+		test(DeviceFeature.CPU);
 	}
 	
+	/*
+	 * TEST JavaCL with GPU
+	 * */
 //	@Test
-//	public void testKernelCPU() throws Exception
-//	{
-//		test(DeviceFeature.CPU);
-//	}
-//	
-//	@Test
-//	public void testKernelGPU() throws Exception
-//	{
+//	public void testKernelGPU() throws Exception {
 //		test(DeviceFeature.GPU);
 //	}
-//	
-//	@After
-//	public void cleanup()
-//	{
-//		System.out.println("Cleanup");
-//		_queue.finish();
-//		_context.release();
-//	}
-	
+
+	public class FakeSolver{
+		private CLContext _context;
+		public CLQueue _queue;
+		private CLProgram _program;
+		private CLDevice _device;
+		private CLKernel _test_kernel;
+		public CLBuffer<Float> _buffer1;
+		public CLBuffer<Float> _buffer2;
+		private Pointer<Float> _buffer1Ptr;
+		private Pointer<Float> _buffer2Ptr;
+		private int SIZE;
+		
+		private float b1[] = null;
+		private float b2[] = null;
+		
+		public FakeSolver(float[] buff1, float[] buff2, int size, DeviceFeature device) throws IOException {
+			this.SIZE = size;
+			this.b1 = buff1;
+			this.b2 = buff2;
+			
+			initializeCL(device);
+		}
+		
+		private void initializeCL(DeviceFeature device) throws IOException {
+			_context = JavaCL.createBestContext(device);
+			ByteOrder byteOrder = _context.getByteOrder();
+
+			out.println("created " + _context);
+			// an array with available devices
+			CLDevice[] devices = _context.getDevices();
+
+			for (int i = 0; i < devices.length; i++) {
+				out.println("device - " + i + ": " + devices[i]);
+			}
+
+			// have a look at the output and select a device
+			_device = devices[0];
+			out.println("Version " + _device.getOpenCLVersion());
+			out.println("Version " + _device.getDriverVersion());
+			out.println("using " + _device);
+			out.println("max workgroup size: " + _device.getMaxWorkGroupSize());
+			out.println("max workitems size: " + _device.getMaxWorkItemSizes()[0]);
+
+			// create command queue on selected device.
+			_queue = _context.createDefaultQueue();
+
+			// load sources, create and build program
+			String src = IOUtils.readText(SPHSolverService.class.getResourceAsStream("/testKernel.cl"));
+			_program = _context.createProgram(src);
+
+			_test_kernel = _program.createKernel("test_kernel");
+
+			_buffer1Ptr = Pointer.allocateFloats(SIZE).order(byteOrder);
+			_buffer2Ptr = Pointer.allocateFloats(SIZE).order(byteOrder);
+			_buffer1 = _context.createBuffer(Usage.Input, _buffer1Ptr, false);
+			_buffer2 = _context.createBuffer(Usage.Output, _buffer2Ptr, false);
+
+			_queue.finish();
+			
+			// populate buffers
+			for (int i = 0; i < SIZE; i++) {
+				_buffer1Ptr.set(i, b1[i]);
+				_buffer2Ptr.set(i, b2[i]);
+			}
+		}
+		
+		public float[] solve(){	
+			// the kernel copies the first buffer in the second one
+			_test_kernel.setArg(0, _buffer1);
+			_test_kernel.setArg(1, _buffer2);
+			_test_kernel.setArg(2, SIZE);
+			_test_kernel.enqueueNDRange(_queue, new int[] { SIZE });
+
+			_buffer2Ptr = _buffer2.read(_queue);
+			
+			float[] results = new float[SIZE];
+			for (int i = 0; i < SIZE; i++) {
+				results[i] = _buffer2Ptr.get(i);
+			}
+			
+			return results;
+		}	
+	}
 }
--- src/test/resources/testKernel.cl
+++ src/test/resources/testKernel.cl
@@ -1,10 +1,17 @@
-
 //#pragma OPENCL EXTENSION cl_amd_printf : enable
 //#pragma OPENCL EXTENSION cl_intel_printf : enable
 
-
-__kernel void test_kernel(__global float4 * in,
-                          __global float4 * out){
+__kernel void test_kernel(__global float * in,
+                          __global float * out,
+                          int SIZE)
+{    
+    // get index into global data array
     int id = get_global_id(0);
+
+    // bound check (equivalent to the limit on a 'for' loop for standard/serial C code
+    if (id >= SIZE)  {
+        return;
+    }
+    
     out[id] = in[id];
 }
