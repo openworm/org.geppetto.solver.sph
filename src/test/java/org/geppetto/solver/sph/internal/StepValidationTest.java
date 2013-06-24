9d966e04e803a3a31d937a9bf35330afcfa80abc
johnidol <giovanni.idili@gmail.com>
2013-06-24 17:51:10 +0100

    added validation test for elastic scene (doesn't pass)

--- src/test/java/org/geppetto/solver/sph/internal/StepValidationTest.java
+++ src/test/java/org/geppetto/solver/sph/internal/StepValidationTest.java
@@ -1,6 +1,37 @@
-package org.geppetto.solver.sph.internal;
+/*******************************************************************************
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2011, 2013 OpenWorm.
+ * http://openworm.org
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/MIT
+ *
+ * Contributors:
+ *     	OpenWorm - http://openworm.org/people.html
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *******************************************************************************/
 
-import static org.junit.Assert.*;
+package org.geppetto.solver.sph.internal;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -26,7 +57,7 @@ import org.junit.Test;
 public class StepValidationTest {
 
 	@Test
-	public void ValidateLiquidScene_16974() throws Exception {
+	public void testValidateLiquidScene16974() throws Exception {
 		// load reference values at various steps from C++ version
 		String position1 = readFile(StepValidationTest.class.getResource("/results/liquid_16974/position_log_1.txt").getPath());
 		String position5 = readFile(StepValidationTest.class.getResource("/results/liquid_16974/position_log_5.txt").getPath());
@@ -82,8 +113,59 @@ public class StepValidationTest {
 	}
 	
 	@Test
-	public void ValidateElasticScene_SingleBundle() throws Exception {
-		fail("Not yet implemented");
+	public void testValidateElasticScene_SingleBundle() throws Exception {
+		// load reference values at various steps from C++ version
+		String position1 = readFile(StepValidationTest.class.getResource("/results/elastic/position_log_1.txt").getPath());
+		String position5 = readFile(StepValidationTest.class.getResource("/results/elastic/position_log_5.txt").getPath());
+		String position10 = readFile(StepValidationTest.class.getResource("/results/elastic/position_log_10.txt").getPath());
+		
+		Map<Integer, String[]> referenceValuesMap = new HashMap<Integer, String[]>();
+		referenceValuesMap.put(1, position1.split(System.getProperty("line.separator")));
+		referenceValuesMap.put(5, position5.split(System.getProperty("line.separator")));
+		referenceValuesMap.put(10, position10.split(System.getProperty("line.separator")));
+		
+		// load Java generated scene
+		URL url = this.getClass().getResource("/sphModel_Elastic.xml");
+		SPHModelInterpreterService modelInterpreter = new SPHModelInterpreterService();
+		SPHModelX model = (SPHModelX)modelInterpreter.readModel(url);
+		
+		// 3. assert number of particles is fine
+		for (Map.Entry<Integer, String[]> entry : referenceValuesMap.entrySet())
+		{
+		    Assert.assertTrue("number of lines on positions and number of particles on sphModel do not match", model.getParticles().size() == entry.getValue().length);
+		}
+		
+		SPHSolverService solver = new SPHSolverService();
+		solver.initialize(model);
+		
+		Map<Integer,Set<Integer>> mismatchingSetsMap = new HashMap<Integer, Set<Integer>>();
+		for(int i = 0; i < 10; i++)
+		{
+			// calculate step
+			StateTreeRoot stateSet = solver.solve(new TimeConfiguration(0.1f, 1, 1));
+			
+			// get reference values
+			int step = i + 1;
+			String[] referenceValues = referenceValuesMap.get(step);
+			
+			checkStateTreeForNaN(stateSet, false);
+			
+			if( referenceValuesMap.containsKey(step) )
+			{
+				// compare state tree with logged values for each recorded step
+				CompareStateVisitor compareVisitor = new CompareStateVisitor(referenceValues);
+				stateSet.apply(compareVisitor);
+				mismatchingSetsMap.put(step, compareVisitor.getMismatches());
+			}
+		}
+		
+		// check mismatching stuff
+		for (Map.Entry<Integer,Set<Integer>> entry : mismatchingSetsMap.entrySet())
+		{
+			Integer step = entry.getKey();
+			// assert there are no mismatching values
+			Assert.assertTrue(entry.getValue().size() + " mismatching values found on step " + step, entry.getValue().size() == 0);
+		}
 	}
 	
 	private String readFile(String path) throws IOException
